5d5fb3ac3a5cdd00c0c81c84dcca03b11da964a9
Akshatha Kunder <kunderakshatha5@gmail.com>
2026-04-07 20:15:10 +0530

    Created USERPOJO and added equals method

--- src/test/java/com/api/pojo/UserPOJO.java
+++ src/test/java/com/api/pojo/UserPOJO.java
@@ -1,5 +1,42 @@
 package com.api.pojo;
 
-public class UserPOJO {
+import java.util.Objects;
 
+public class UserPOJO {
+	private String name;
+
+	public UserPOJO(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "USERPOJO [name=" + name + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserPOJO other = (UserPOJO) obj;
+		return Objects.equals(name, other.name);
+	}
 }
